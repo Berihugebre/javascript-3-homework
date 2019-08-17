55be26421c415c78b76471a708c014d02e08e768
berihugebre <danigebre039@gmail.com>
2019-08-17 16:24:39 +0200

    second commit

--- Project/app.js
+++ Project/app.js
@@ -1,33 +1,36 @@
 const buttonOne = document.getElementById('btn-1');
 buttonOne.addEventListener('click', getCountries);
+const loader = document.getElementById('loader');
 const root = document.getElementById('root');
-function getCountries() {
-  const fetchAll = fetch('https://restcountries.eu/rest/v2/all');
-  fetchAll.then(response => response.json())
-  .then(function (country) {
+
+function getCountries () {
+  loader.style ='display : inherit'
+  fetch('https://restcountries.eu/rest/v2/all')
+  .then(response => response.json())
+  .then(function (countries) {
+    loader.style = 'display : inherit';
     buttonOne.setAttribute('disabled' ,'');
-    const select = createAndAppend('select', root, { id: 'countries', class : 'custom-select custom-select-sm' });
-    for (let i = 0; i < country.length; i++){
-      createAndAppend('option',select, {text:`${country[i].name}`, value: `${i}`})
-      }
-    const containerOne = createAndAppend('div', root , {class: "card"});
+    const select = createAndAppend('select', root, { id: 'countries'});
+    for (let i = 0; i < countries.length; i++){
+      createAndAppend('option',select, {text:`${countries[i].name}`, value: `${i}`})
+    }
+    loader.style = 'display : none';
+    const containerOne = createAndAppend('div', root);
     const selector = document.getElementById('countries');
     selector.addEventListener('change', () => {
+      loader.style = 'display : none';
       while (containerOne.firstChild) {
         containerOne.removeChild(containerOne.firstChild);
       }
       const index = selector.value;
-      const flags = createAndAppend('img', containerOne, { src: `${country[index].flag}` , class : 'card-img-top' }) 
-      const aboutCountry = createAndAppend('p', containerOne,
-        { class : 'card-text',
-          text: `The selected Country is ${country[index].name}
-                its Capitals city is ${country[index].capital}
-                and it has population of ${country[index].population} peoples
-                and its Bordering countries are ${country[index].borders}`
-        })
+      createAndAppend('img', containerOne, { src: `${countries[index].flag}` }) 
+      createTable('Country', `${countries[index].name}`, containerOne);
+      createTable('Capital City', `${countries[index].capital}`, containerOne);
+      createTable('population', `${countries[index].population}`, containerOne);
+      createTable(' Borders', `${countries[index].borders}` , containerOne);
   })
-
   }).catch(error => {
+    loader.style = 'display : none';
     const errorMessage = new Error(`Network error : ${error.status} - ${error.statusText}`);
     createAndAppend('div', root, { text: `${errorMessage}` , style: 'color: red;'});
 })
@@ -46,4 +49,12 @@ function createAndAppend (name, parent, options ={}) {
     }
   })
   return elem
+}
+
+function createTable (tableHeader, tableData , parent) {
+  const table = createAndAppend('table', parent, {class: 'table'});
+  const tr = createAndAppend('tr', table);
+  createAndAppend('th', tr, { text: `${tableHeader}`, scope: "col" });
+  createAndAppend('td', tr, { text: `${tableData}` ,scope: "row"});
+  return table; 
 }
