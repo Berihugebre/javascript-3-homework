a24f6852409b1bd0808f56d91a4a3896250b550f
berihugebre <danigebre039@gmail.com>
2019-08-19 10:16:04 +0200

    splitin the function

--- Project/app.js
+++ Project/app.js
@@ -2,42 +2,52 @@ const buttonOne = document.getElementById('btn-1');
 buttonOne.addEventListener('click', getCountries);
 const loader = document.getElementById('loader');
 const root = document.getElementById('root');
+createAndAppend('select',root, {id:'countries'})
+const selector = document.getElementById('countries');
+const containerOne = createAndAppend('div', root);
+let allCountries = [];
+function fetchJSON (url) {
+ return fetch(url).then(response => response.json())
+    .then(data => data);
+}
+console.log(fetchJSON('https://pokeapi.co/api/v2/pokemon/'));
 
-function getCountries () {
-  loader.style ='display : inherit'
+function getCountries() {
+  loader.style = 'display : inherit';
   fetch('https://restcountries.eu/rest/v2/all')
-  .then(response => response.json())
-  .then(function (countries) {
-    loader.style = 'display : inherit';
-    buttonOne.setAttribute('disabled' ,'');
-    const select = createAndAppend('select', root, { id: 'countries'});
-    for (let i = 0; i < countries.length; i++){
-      createAndAppend('option',select, {text:`${countries[i].name}`, value: `${i}`})
-    }
-    loader.style = 'display : none';
-    const containerOne = createAndAppend('div', root);
-    const selector = document.getElementById('countries');
-    selector.addEventListener('change', () => {
-      loader.style = 'display : none';
-      while (containerOne.firstChild) {
-        containerOne.removeChild(containerOne.firstChild);
+    .then(response => response.json())
+    .then(function(countries) {
+      allCountries = countries;
+      loader.style = 'display : inherit';
+      buttonOne.setAttribute('disabled', '');
+      for (let i = 0; i < countries.length; i++) {
+        createAndAppend('option', selector, { text: `${countries[i].name}`, value: `${i}` });
       }
-      const index = selector.value;
-      createAndAppend('img', containerOne, { src: `${countries[index].flag}` }) 
-      createTable('Country', `${countries[index].name}`, containerOne);
-      createTable('Capital City', `${countries[index].capital}`, containerOne);
-      createTable('population', `${countries[index].population}`, containerOne);
-      createTable(' Borders', `${countries[index].borders}` , containerOne);
-  })
-  }).catch(error => {
-    loader.style = 'display : none';
-    const errorMessage = new Error(`Network error : ${error.status} - ${error.statusText}`);
-    createAndAppend('div', root, { text: `${errorMessage}` , style: 'color: red;'});
-})
-};
+      loader.style = 'display : none';
+    })
+    .catch(error => {
+      loader.style = 'display : none';
+      const errorMessage = new Error(`Network error : ${error.status} - ${error.statusText}`);
+      createAndAppend('div', root, { text: `${errorMessage}`, style: 'color: red;' });
+    });
+}
 
-function createAndAppend (name, parent, options ={}) {
-  const  elem = document.createElement(name);
+selector.addEventListener('change', () => {
+  loader.style = 'display : none';
+  while (containerOne.firstChild) {
+    containerOne.removeChild(containerOne.firstChild);
+  }
+  const index = selector.value;
+  const selectedCountry = allCountries[index];    
+  createAndAppend('img', containerOne, { src: `${selectedCountry.flag}` });
+  createTable('Country', `${selectedCountry.name}`, containerOne);
+  createTable('Capital City', `${selectedCountry.capital}`, containerOne);
+  createTable('population', `${selectedCountry.population}`, containerOne);
+  createTable(' Borders', `${selectedCountry.borders}`, containerOne);
+});
+
+function createAndAppend(name, parent, options = {}) {
+  const elem = document.createElement(name);
   parent.appendChild(elem);
   const keys = Object.keys(options);
   keys.forEach(key => {
@@ -45,16 +55,16 @@ function createAndAppend (name, parent, options ={}) {
     if (key === 'text') {
       elem.textContent = value;
     } else {
-      elem.setAttribute(key, value)
+      elem.setAttribute(key, value);
     }
-  })
-  return elem
+  });
+  return elem;
 }
 
-function createTable (tableHeader, tableData , parent) {
-  const table = createAndAppend('table', parent, {class: 'table'});
+function createTable(tableHeader, tableData, parent) {
+  const table = createAndAppend('table', parent, { class: 'table' });
   const tr = createAndAppend('tr', table);
-  createAndAppend('th', tr, { text: `${tableHeader}`, scope: "col" });
-  createAndAppend('td', tr, { text: `${tableData}` ,scope: "row"});
-  return table; 
+  createAndAppend('th', tr, { text: `${tableHeader}`, scope: 'col' });
+  createAndAppend('td', tr, { text: `${tableData}`, scope: 'row' });
+  return tr;
 }
